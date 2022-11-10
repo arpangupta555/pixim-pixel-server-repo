f4eb62d687a99330349da24f41f4a0a86c63dee2
ArpanGupta707 <arpangupta707@gmail.com>
2022-11-10 11:42:20 +0600

    sixth commit

--- index.js
+++ index.js
@@ -58,8 +58,19 @@ async function run() {
             const review = req.body;
             const result = await reviewCollection.insertOne(review);
             res.send(result);
+        })
+
+        app.delete('/review/:id', async (req, res) => {
+
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await reviewCollection.deleteOne(query);
+            res.send(result);
+
 
         })
+
+
 
     }
     finally {
